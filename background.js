d46b5f6ec0cf3c675490b1ebbe7f5a99976d5c0c
Erhan Alankus <e.alankus@gmail.com>
2020-08-05 21:12:47 +0300

    Add India to Initially Hidden Countries

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
 chrome.runtime.onInstalled.addListener(function() {
-    let initiallyHiddenCountries = ["Afghanistan", "Bangladesh"];
+    let initiallyHiddenCountries = ["Afghanistan", "Bangladesh", "India"];
     chrome.storage.sync.set({ hiddenCountries: initiallyHiddenCountries });
 });
