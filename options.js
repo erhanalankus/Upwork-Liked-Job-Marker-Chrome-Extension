e1f3c66848da68d148fb44a6381607e113141f3a
Erhan Alankus <e.alankus@gmail.com>
2020-08-05 21:13:43 +0300

    Get the Country Name to removeCountryFromBlackList Method

--- options.js
+++ options.js
@@ -5,6 +5,26 @@ function setHideFunctionality() {
     });
 }
 
+function setRemoveFunctionality() {
+    let buttonsArray;
+    setTimeout(() => {
+        buttonsArray = convertToArray();
+        buttonsArray.forEach(button => {
+            button.addEventListener('click', function() {
+                removeCountryFromBlackList(button.getAttribute("countryName"));
+            });
+        });
+    });
+
+    function convertToArray() {
+        let removeCountryButtons = document.getElementsByClassName("remove-country-button");
+        let array = Array.from(removeCountryButtons);
+        return array;
+    }
+
+
+}
+
 function listHiddenCountries() {
     chrome.storage.sync.get('hiddenCountries', function(data) {
         let hiddenCountriesList = document.getElementById("hiddenCountriesList");
@@ -12,13 +32,24 @@ function listHiddenCountries() {
             let listItem = document.createElement("li");
             listItem.appendChild(document.createTextNode(countryName));
             hiddenCountriesList.appendChild(listItem);
+
+            let removeButton = document.createElement("button");
+            removeButton.setAttribute("countryName", countryName);
+            removeButton.setAttribute("class", "remove-country-button");
+            removeButton.appendChild(document.createTextNode("Remove"));
+            listItem.appendChild(removeButton);
         });
     });
 }
 
 function addCountryToBlacklist(countryName) {
-    alert(countryName.value);
+    alert('add ' + countryName.value);
+}
+
+function removeCountryFromBlackList(countryName) {
+    alert('remove ' + countryName)
 }
 
 setHideFunctionality();
 listHiddenCountries();
+setRemoveFunctionality();
