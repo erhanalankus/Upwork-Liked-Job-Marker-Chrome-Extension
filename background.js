b5b7aed0cd4423c8102eb8a524107b412d9184c4
Erhan Alankus <erhan.alankus@algomedi.com>
2020-12-06 13:03:48 +0300

    Add Countries to Blacklist

--- background.js
+++ background.js
@@ -1,4 +1,16 @@
 chrome.runtime.onInstalled.addListener(function() {
-    let initiallyHiddenCountries = ["Afghanistan", "Bangladesh", "India"];
+    let initiallyHiddenCountries = [
+        "Afghanistan", 
+        "Bangladesh", 
+        "India",
+        "United Arab Emirates",
+        "Philippines",
+        "Nepal",
+        "Romania",
+        "Saudi Arabia",
+        "Cyprus",
+        "Pakistan",
+        
+    ];
     chrome.storage.sync.set({ hiddenCountries: initiallyHiddenCountries });
 });
