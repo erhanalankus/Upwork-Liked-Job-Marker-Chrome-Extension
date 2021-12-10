5b20f3847a0da357f9b39f10d9f5138996e7e4c7
Erhan Alankus <e.alankus@gmail.com>
2021-12-10 17:44:18 +0300

    Mark Liked Jobs Light Green

--- on-scroll-actions.js
+++ on-scroll-actions.js
@@ -1,3 +1,17 @@
 window.onscroll = function() {
-    
+    let heartIcons = document.getElementsByClassName("up-icon");
+
+    for (let heart of heartIcons) {        
+        let pathNode = heart.getElementsByTagName("path")[0];
+        if (!pathNode) {
+            continue;
+        }
+        let dValueOfPathElement = pathNode.getAttribute("d");
+        if (!dValueOfPathElement) {
+            continue
+        }
+        if (dValueOfPathElement[1] == "7") {
+            heart.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.style.backgroundColor = "lightgreen";
+        }
+    }
 }
